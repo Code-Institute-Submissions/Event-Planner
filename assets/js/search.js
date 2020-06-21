da687b5b5100360848148e6f7805336cd1d9d70e
Karl <karltimmins@gmail.com>
2020-06-21 13:59:43 +0000

    Addition of search.js + updated maps locations + index.html nav bar amendment to include search content

---
+++ assets/js/search.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+        $("#search").keyup(function(){
+            var searchText = $(this).val();
+            if (searchText!=''){
+                $.ajax({
+                    url:'action.php',
+                    method:post,
+                    data:{query:searchText},
+                    success:function(response){
+                        $('#show-list').html(response);
+                    }
+                });
+            }
+            else{
+                $("#show-list").html('');
+            }
+        });
+        $(document).on('click', 'a', function(){
+            $("#search").val($(this).text());
+            $("#show-list").html('');
+        });
+    });
